c92b99f2eb783e513e0ce6a83666ce394c890dc0
Robert Schroeder <robschroeder333@users.noreply.github.com>
2017-03-10 09:24:59 -0500

    initialized repo

---
+++ HandState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandState : MonoBehaviour {
+
+
+	// void Awake () {
+	// 	hand = GameObject.Find("HandController");
+	// 	HC = handControllerObject.GetComponent<HandController>() as HandController;
+	// }
+	// Use this for initialization
+	private HandModel HM;
+	void Start () {
+		HM = GetComponent<HandModel>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(HM.GetLeapHand().Fingers);
+	}
+}
---
+++ ViewController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewController : MonoBehaviour {
+
+private HandController HC;
+private GameObject handControllerObject;
+
+
+
+//set script on the hand prefabs to return boolean true if
+//no fingers are extended.
+
+//object find them here in update (like in other script)
+
+//get component of each to access the function
+
+//if both return true then get the values of positions
+//and continue working
+
+	void Awake () {
+		// handControllerObject = GameObject.Find("HandController");
+
+		HC = this.gameObject.GetComponent<HandController>() as HandController;
+	}
+	void Start () {
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Debug.Log(HC.GetAllGraphicsHands());
+	}
+}
