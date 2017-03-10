3b4da5cf9600b734270f4daf6248a05385b1fe47
Robert Schroeder <robschroeder333@users.noreply.github.com>
2017-03-10 11:32:17 -0500

    IsFist up, tracking debugging now

--- HandState.cs
+++ HandState.cs
@@ -1,22 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Leap;
+// using Leap.Unity;
 
 public class HandState : MonoBehaviour {
 
-
-	// void Awake () {
-	// 	hand = GameObject.Find("HandController");
-	// 	HC = handControllerObject.GetComponent<HandController>() as HandController;
-	// }
-	// Use this for initialization
 	private HandModel HM;
-	void Start () {
-		HM = GetComponent<HandModel>();
+	private Hand hand;
+
+	public bool IsFist (Hand hand) {
+		for (int f = 0; f < hand.Fingers.Count; f++) {
+			Finger digit = hand.Fingers [f];
+			if (digit.IsExtended) {
+					return false;
+			}
+		}
+		return true;
 	}
 
-	// Update is called once per frame
-	void Update () {
-		Debug.Log(HM.GetLeapHand().Fingers);
+	void Start () {
+		HM = this.gameObject.GetComponent<HandModel>();
+	}
+
+	void FixedUpdate () {
+		hand = HM.GetLeapHand();
+		Debug.Log(IsFist(hand));
 	}
 }
