3cc6d87925c9e876738ac93b737eae73b4f1b63f
Robert Schroeder <robschroeder333@users.noreply.github.com>
2017-03-12 23:00:12 -0400

    scaling, shifting and single axis rotation are done

--- ViewController.cs
+++ ViewController.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Leap;
 
 public class ViewController : MonoBehaviour {
 
 // script imports
-// private HandController HandControllerScript;
-// private SkeletalHand SkeletalHandScript1;
 private HandState handScriptL;
 private HandState handScriptR;
 
 // var declaration
-// private GameObject handController;
 private GameObject handL;
 private GameObject handR;
 private Vector3 leftPos;
@@ -21,27 +19,27 @@ private bool isStartDist;
 private float startDist;
 private float currentDist;
 private GameObject gameCube;
-private bool shouldUpdateGameCube;
+private bool shouldUpdateGameCubeScale;
 public float scaleRate = 50f;
+private bool isStartRot;
+private Vector3 startRot;
+private Vector3 currentRot;
+private bool shouldUpdateGameCubeRot;
+private bool isStartPos;
+private Vector3 startPos;
+private Vector3 currentPos;
+private bool shouldUpdateGameCubePos;
+public float posRate = 50f;
 
 /*TODO:
-	rotate cube
-		conflict in order of vectors passed (hands first = y rotation correct
-		but z rotation reversed, hands second = y reversed but z correct)
+	-rotate cube
+		y is good -z is good but combined is acting strange
 
-	shift cube
-
-	create better model for cube
+	-create better model for cube
 
 
 */
 
-
-
-	// void Awake () {
-	// 	// handController = GameObject.Find("HandController");
-	// 	// HandControllerScript = handController.GetComponent<HandController>() as HandController;
-	// }
 	void Start () {
 		gameCube = GameObject.Find("GameSpace");
 	}
@@ -49,8 +47,15 @@ public float scaleRate = 50f;
 	// Update is called once per frame
 	void Update () {
 
-			handL = GameObject.Find("GlowRobotLeftHand(Clone)");
-			handR = GameObject.Find("GlowRobotRightHand(Clone)");
+		if (Input.GetKey("escape")) {
+			Application.Quit();
+		}
+		if (Input.GetKey("space")) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+
+		handL = GameObject.Find("GlowRobotLeftHand(Clone)");
+		handR = GameObject.Find("GlowRobotRightHand(Clone)");
 
 		if (handL) {
 			handScriptL = handL.GetComponent<HandState>() as HandState;
@@ -61,6 +66,8 @@ public float scaleRate = 50f;
 
 		if (handL && handR) {
 			if (handScriptL.IsFist() && handScriptR.IsFist()) {
+				isStartPos = true;
+
 				leftPos = new Vector3(
 					handScriptL.hand.PalmPosition.x,
 					handScriptL.hand.PalmPosition.y,
@@ -72,12 +79,44 @@ public float scaleRate = 50f;
 					handScriptR.hand.PalmPosition.z
 				);
 
-				// HandleScaling();
+				HandleScaling();
 				HandleRotation();
 
+			} else if (handScriptL.IsFist()) {
+				leftPos = new Vector3(
+					handScriptL.hand.PalmPosition.x,
+					handScriptL.hand.PalmPosition.y,
+					handScriptL.hand.PalmPosition.z
+				);
+				HandleShifting(leftPos);
+			} else if (handScriptR.IsFist()) {
+				rightPos = new Vector3(
+					handScriptR.hand.PalmPosition.x,
+					handScriptR.hand.PalmPosition.y,
+					handScriptR.hand.PalmPosition.z
+				);
+				HandleShifting(rightPos);
 			} else {
+				isStartPos = true;
 				isStartDist = true;
+				isStartRot = true;
 			}
+		} else if (handL && handScriptL.IsFist()) {
+				leftPos = new Vector3(
+					handScriptL.hand.PalmPosition.x,
+					handScriptL.hand.PalmPosition.y,
+					handScriptL.hand.PalmPosition.z
+				);
+				HandleShifting(leftPos);
+		} else if (handR && handScriptR.IsFist()) {
+				rightPos = new Vector3(
+					handScriptR.hand.PalmPosition.x,
+					handScriptR.hand.PalmPosition.y,
+					handScriptR.hand.PalmPosition.z
+				);
+				HandleShifting(rightPos);
+		} else {
+			isStartPos = true;
 		}
 
 
@@ -93,20 +132,40 @@ public float scaleRate = 50f;
 		if (isStartDist) {
 			startDist = Vector3.Distance(leftPos, rightPos);
 			isStartDist = false;
-			shouldUpdateGameCube = true;
+			shouldUpdateGameCubeScale = true;
 		} else {
 			currentDist = Vector3.Distance(leftPos, rightPos);
-			float cubeScale = (currentDist-startDist)/scaleRate;
+			float cubeScale = (currentDist - startDist) / scaleRate;
 			startDist = currentDist;
 			gameCube.transform.localScale += new Vector3(cubeScale,cubeScale,cubeScale);
 		}
 	}
 
 	void HandleRotation () {
-		gameCube.transform.rotation = Quaternion.FromToRotation(
-			(rightPos - leftPos),Vector3.right
-		);
+		if (isStartRot) {
+			startRot = rightPos - leftPos;
+			isStartRot = false;
+			shouldUpdateGameCubeRot = true;
+		} else {
+			currentRot = rightPos - leftPos;
+			Vector3 cubeRot = (currentRot - startRot);
+			startRot = currentRot;
+			// gameCube.transform.Rotate(0f, cubeRot.z, cubeRot.y); acting strange
+			gameCube.transform.Rotate(0, cubeRot.z, 0);
+		}
 	}
 
+	void HandleShifting (Vector3 handPos) {
+		if (isStartPos) {
+			startPos = handPos;
+			isStartPos = false;
+			shouldUpdateGameCubePos = true;
+		} else {
+			currentPos = handPos;
+			Vector3 cubeShift = (currentPos - startPos) / posRate;
+			startPos = currentPos;
+			gameCube.transform.position += new Vector3(cubeShift.x, cubeShift.y, -cubeShift.z);
+		}
+	}
 
 }
